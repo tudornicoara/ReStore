beb90ed8abd9ab30d0027d43c02acafd89617170
Tudor Nicoara <nicoaratudor@gmail.com>
2022-03-11 12:54:16 +0200

    Added a loading indicator and not found page

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,11 +1,15 @@
 import axios, {AxiosError, AxiosResponse} from "axios";
 import {toast} from "react-toastify";
+import {history} from "../../index";
+
+const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-axios.interceptors.response.use(response => {
+axios.interceptors.response.use(async response => {
+    await sleep();
     return response;
 }, (error: AxiosError) => {
     const {data, status} = error.response!;
@@ -26,7 +30,10 @@ axios.interceptors.response.use(response => {
             toast.error(data.title);
             break;
         case 500:
-            toast.error(data.title);
+            history.push({
+                pathname: '/server-error',
+                state: {error: data}
+            });
             break;
         default:
             break;
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import Header from "./Header";
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
@@ -9,6 +9,8 @@ import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -32,11 +34,15 @@ function App() {
         <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
         <Container>
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/catalog' component={Catalog} />
-            <Route path='/catalog/:id' component={ProductDetails} />
-            <Route path='/about' component={AboutPage} />
-            <Route path='/contact' component={ContactPage} />
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route exact path='/catalog' component={Catalog} />
+                <Route path='/catalog/:id' component={ProductDetails} />
+                <Route path='/about' component={AboutPage} />
+                <Route path='/contact' component={ContactPage} />
+                <Route path='/server-error' component={ServerError} />
+                <Route component={NotFound} />
+            </Switch>
         </Container>
     </ThemeProvider>
     );
