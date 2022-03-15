5a52528ac467e3f5c3e6b6747690ca9c296435dd
Tudor Nicoara <nicoaratudor@gmail.com>
2022-03-15 19:04:48 +0200

    Switched from react contexts to Redux

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {
     Avatar,
     Button,
@@ -10,26 +10,18 @@ import {
 } from "@mui/material";
 import {Product} from "../../app/models/product";
 import {Link} from "react-router-dom";
-import agent from "../../app/api/agent";
 import {LoadingButton} from "@mui/lab";
-import {useStoreContext} from "../../app/context/StoreContext";
 import {currencyFormat} from "../../app/util/util";
+import {useAppDispatch, useAppSelector} from "../../app/store/configureStore";
+import {addBasketItemAsync} from "../basket/basketSlice";
 
 interface Props {
     product: Product;
 }
 
 export default function ProductCard({product}: Props) {
-    const [loading, setLoading] = useState(false);
-    const {setBasket} = useStoreContext();
-    
-    function handleAddItem(productId: number) {
-        setLoading(true);
-        agent.Basket.addItem(productId)
-            .then(basket => setBasket(basket))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false))
-    }
+    const {status} = useAppSelector(state => state.basket);
+    const dispatch = useAppDispatch();
     
     return (
         <Card>
@@ -58,7 +50,11 @@ export default function ProductCard({product}: Props) {
                 </Typography>
             </CardContent>
             <CardActions>
-                <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">
+                <LoadingButton
+                    size="small"
+                    loading={status.includes('pendingAddItem' + product.id)}
+                    onClick={() => dispatch(addBasketItemAsync({ productId: product.id}))}
+                >
                     Add to cart
                 </LoadingButton>
                 <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,16 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import App from "./app/layout/App";
 import {createBrowserHistory} from "history";
 import {Router} from "react-router-dom";
-import {StoreProvider} from "./app/context/StoreContext";
+import {Provider} from "react-redux";
+import {store} from "./app/store/configureStore";
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
       <Router history={history}>
-          <StoreProvider>
-              <App /> 
-          </StoreProvider>
+          <Provider store={store}>
+              <App />
+          </Provider>
       </Router>
   </React.StrictMode>,
   document.getElementById('root')
