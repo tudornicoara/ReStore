67a69a8002e0de436032e8ccb57f02df526f2fbd
ENDAVA\tunicoara <Tudor.Nicoara@endava.com>
2022-04-14 15:35:33 +0300

    React Hook Form Login

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -75,10 +75,17 @@ const Basket = {
     
 }
 
+const Account = {
+    login: (values: any) => requests.post('account/login', values),
+    register: (values: any) => requests.post('account/register', values),
+    currentUser: () => requests.get('account/currentUser')
+}
+
 const agent = {
     Catalog,
     TestErrors,
-    Basket
+    Basket,
+    Account
 }
 
 export default agent;
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -18,6 +18,8 @@ import LoadingComponent from "./LoadingComponent";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
 import {useAppDispatch} from "../store/configureStore";
 import {setBasket} from "../../features/basket/basketSlice";
+import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
 
 function App() {
     const dispatch = useAppDispatch();
@@ -66,6 +68,8 @@ function App() {
                 <Route path='/contact' component={ContactPage} />
                 <Route path='/basket' component={BasketPage} />
                 <Route path='/checkout' component={CheckoutPage} />
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
                 <Route path='/server-error' component={ServerError} />
                 <Route component={NotFound} />
             </Switch>
