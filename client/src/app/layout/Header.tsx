2e5a9d8992aee2e1339d256aab705f1b9899b315
ENDAVA\tunicoara <Tudor.Nicoara@endava.com>
2022-04-14 16:44:36 +0300

    Persisting login with interceptors

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import {AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typogra
 import {DarkMode, LightMode, ShoppingCart} from "@mui/icons-material";
 import {Link, NavLink} from "react-router-dom";
 import {useAppSelector} from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 interface Props {
     darkMode: boolean;
@@ -33,6 +34,7 @@ const navStyles = {
 
 export default function Header({darkMode, handleThemeChange}: Props) {
     const {basket} = useAppSelector(state => state.basket);
+    const {user} = useAppSelector(state => state.account);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
     
     return (
@@ -62,20 +64,24 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
-                    
-                    <List sx={{display: 'flex'}}>
-                        {rightLinks.map(({title, path}) => (
-                            <ListItem
-                                component={NavLink}
-                                to={path}
-                                key={path}
-                                sx={navStyles}
-                            >
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
 
+                    {user ? (
+                        <SignedInMenu />
+                    ) : (
+                        <List sx={{display: 'flex'}}>
+                            {rightLinks.map(({title, path}) => (
+                                <ListItem
+                                    component={NavLink}
+                                    to={path}
+                                    key={path}
+                                    sx={navStyles}
+                                >
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))}
+                        </List>
+                    )}
+                    
                     <Switch checked={darkMode} onChange={handleThemeChange}
                             icon={<LightMode sx={{bgcolor: 'grey.700', borderRadius: 4}} />}
                             checkedIcon={<DarkMode sx={{bgcolor: 'grey.700', borderRadius: 4}} />}
---
+++ client/src/app/layout/SignedInMenu.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {Button, Fade, Menu, MenuItem} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "../store/configureStore";
+import {signOut} from "../../features/account/accountSlice";
+
+export default function SignedInMenu() {
+    const dispatch = useAppDispatch();
+    const {user} = useAppSelector(state => state.account);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <>
+            <Button 
+                color='inherit'
+                onClick={handleClick}
+                sx={{typography: 'h6'}}
+            >
+                {user?.email}
+            </Button>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Fade}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My orders</MenuItem>
+                <MenuItem onClick={() => dispatch(signOut())}>Logout</MenuItem>
+            </Menu>
+        </>
+    );
+}
