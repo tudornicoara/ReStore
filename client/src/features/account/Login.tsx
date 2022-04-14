05ebea01fc7c3db811c8fa72910e1c7f27fd249b
ENDAVA\tunicoara <Tudor.Nicoara@endava.com>
2022-04-14 15:45:13 +0300

    Login validation

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -13,10 +13,16 @@ import {FieldValues, useForm} from "react-hook-form";
 import {LoadingButton} from "@mui/lab";
 
 export default function Login() {
-    const {register, handleSubmit, formState: {isSubmitting}} = useForm();
+    const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
+        mode: 'onTouched'
+    });
     
     async function submitForm(data: FieldValues) {
-        await agent.Account.login(data);
+        try {
+            await agent.Account.login(data);
+        } catch (error) {
+            console.log(error);
+        }
     }
     
     return (
@@ -34,16 +40,21 @@ export default function Login() {
                     fullWidth
                     label="Username"
                     autoFocus
-                    {...register('username')}
+                    {...register('username', {required: 'Username is required'})}
+                    error={!!errors.username}
+                    helperText={errors?.username?.message}
                 />
                 <TextField
                     margin="normal"
                     fullWidth
                     label="Password"
                     type="password"
-                    {...register('password')}
+                    {...register('password', {required: 'Password is required'})}
+                    error={!!errors.password}
+                    helperText={errors?.password?.message}
                 />
                 <LoadingButton
+                    disabled={!isValid}
                     loading={isSubmitting}
                     type="submit"
                     fullWidth
