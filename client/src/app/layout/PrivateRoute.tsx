dc9cc951385296322697b333cc453faf90f04bc9
Tudor Nicoara <nicoaratudor@gmail.com>
2022-05-26 12:41:17 +0300

    Restricting users that are not admin

--- client/src/app/layout/PrivateRoute.tsx
+++ client/src/app/layout/PrivateRoute.tsx
@@ -1,28 +1,31 @@
 import {Redirect, Route, RouteComponentProps, RouteProps} from "react-router-dom";
 import {ComponentType} from "react";
 import {useAppSelector} from "../store/configureStore";
+import {toast} from "react-toastify";
 
 interface Props extends RouteProps{
-    component: ComponentType<RouteComponentProps<any>> | ComponentType<any>
+    component: ComponentType<RouteComponentProps<any>> | ComponentType<any>;
+    roles?: string[];
 }
 
-export default function PrivateRoute({ component: Component, ...rest }: Props) {
+export default function PrivateRoute({ component: Component, roles, ...rest }: Props) {
     const {user} = useAppSelector(state => state.account);
     
     return (
         <Route
             {...rest}
-            render={ props =>
-                user ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect
-                        to={{
-                            pathname: "/login",
-                            state: { from: props.location }
-                        }}
-                    />
-                )
+            render={ props => {
+                    if (!user) {
+                        return <Redirect to={{ pathname: "/login", state: { from: props.location } }} /> 
+                    }
+                    
+                    if (roles && !roles?.some(r => user.roles?.includes(r))) {
+                        toast.error('Not authorised to access this area');
+                        return <Redirect to={{ pathname: "/catalog" }} />
+                    }
+                    
+                    return <Component {...props} />
+                }
             }
         />
     );
